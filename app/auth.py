3e94797f8cdc026d5128ac018933146b38cd2e0b
wazii <muhammedwazeem0807@gmail.com>
2026-06-08 19:40:25 +0530

    Completed JWT authentication module

---
+++ app/auth.py
@@ -0,0 +1,41 @@
+from jose import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "my_super_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
+
+from jose import JWTError
+
+def verify_token(token: str):
+
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        return payload
+
+    except JWTError:
+        return None
